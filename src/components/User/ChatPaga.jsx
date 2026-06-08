2ffe0dc9c39c6150fb7b785296b94a616b10b0a8
Durgaprasad_Kasa <prasadkasa2@gmail.com>
2026-06-08 08:05:42 +0530

    Improving chat ui

--- src/components/User/ChatPaga.jsx
+++ src/components/User/ChatPaga.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getSocket } from "../../Constants";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 const ChatPage = () => {
-    
+  const [message, setMessage] = useState();
+
   const user = useSelector((store) => store.user.profile);
-  const {targetUserId} = useParams();
+  const { targetUserId } = useParams();
 
   const userId = user?._id;
 
@@ -14,11 +15,54 @@ const ChatPage = () => {
     const socket = getSocket();
 
     if (userId) {
-      socket.emit("joinChat", { userName: user?.userName, userId, targetUserId });
+      socket.emit("joinChat", {
+        userName: user?.userName,
+        userId,
+        targetUserId,
+      });
     }
+
+    socket.on("recieveMessage", (data) => {
+      console.log("recieveMessage", data);
+    });
   }, [user]);
 
-  return <div className="w-full sm:min-h-[83vh] bg-[#4a4a4a]"></div>;
+  const sendMessage = () => {
+    console.log(message);
+    const socket = getSocket();
+
+    if (!message.trim()) return;
+
+    socket.emit("sendMessages", {
+      message,
+      userName: user?.userName,
+      userId,
+      targetUserId,
+    });
+  };
+
+  return (
+    <div className="w-full sm:min-h-[83vh] bg-[#4a4a4a] mt-6 flex items-center justify-center">
+      <div className="h-[500px] w-[450px] border border-gray-200">
+        <div className="h-[15%] bg-[#3a3a3a]">
+          <p>{targetUserId}</p>
+        </div>
+        <div className="h-[78%] "></div>
+        <div className="flex items-center justify-center gap-2 px-4">
+          <input
+            type="text"
+            placeholder="type"
+            className="bg-[#3a3a3a]/50 w-full py-1 px-4"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button className="text-white bg-red-600" onClick={sendMessage}>
+            send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatPage;
